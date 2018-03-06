3cbcd2101ba65676339b41131ef4f505d4eb264e
wayabi <wayabi.software@gmail.com>
2018-03-07 01:47:52 +0900

    add if_then rule

--- a.py
+++ a.py
@@ -7,20 +7,56 @@ prefix_ = "##"
 suffix_ = "##"
 out_dir_ = "./out"
 
-def recursive_replace(base_file_name, base_content, brute, index_key, out_dir):
-	key = brute[index_key][0]
+def check_if_then(brute, index_history, if_then):
+	for it in if_then:
+		key = it[0]
+		value = it[1]
+		#print("%s=%s?" % (key, value))
+		for i in range(len(index_history)):
+			if index_history[i] < 0:
+				continue
+			key0 = brute[i][0].split("?")[0]
+			value0 = brute[i][1][index_history[i]][0]
+			if key == key0:
+				if value != value0:
+					return False
+	return True
+
+def recursive_replace(base_file_name, base_content, brute, index_key, index_history, out_dir):
+	ss_key = brute[index_key][0].split("?")
+	key = ss_key[0]
+	if_then = []
+	if len(ss_key) > 1:
+		for a in range(1, len(ss_key)):
+			ss = ss_key[a].split("=")
+			if_then.append((ss[0], ss[1]))
+
+	index_col = -1
+	flag_replace = check_if_then(brute, index_history, if_then)
+	if flag_replace == True:
+		index_col = 0
+
 	for brute0 in brute[index_key][1]:
 		r_content = brute0[0]
 		r_content_file = brute0[1]
-		file_name = base_file_name.replace("%s%s%s" % (prefix_, key, suffix_), r_content_file) 
-		content = base_content.replace("%s%s%s" % (prefix_, key, suffix_), r_content)
+
+		if flag_replace == True:
+			file_name = base_file_name.replace("%s%s%s" % (prefix_, key, suffix_), r_content_file) 
+			content = base_content.replace("%s%s%s" % (prefix_, key, suffix_), r_content)
+		else:
+			file_name = base_file_name
+			content = base_content
 		ik = index_key + 1
 		if ik == len(brute):
 			with open("%s/%s" % (out_dir, file_name), "w") as f:
 				f.write(content)
 				print(file_name)
 		else:
-			recursive_replace(file_name, content, brute, ik, out_dir)
+			new_history = list(index_history)
+			new_history.append(index_col)
+			recursive_replace(file_name, content, brute, ik, new_history, out_dir)
+		if index_col >= 0:
+			index_col = index_col + 1
 
 if __name__ == "__main__":
 
@@ -58,4 +94,5 @@ if __name__ == "__main__":
 
 		print(brute)
 		file_name = ntpath.basename(base_file_path)
-		recursive_replace(file_name, base_content, brute, 0, out_dir_)
+		index_history = []
+		recursive_replace(file_name, base_content, brute, 0, index_history, out_dir_)
--- z.sh
+++ z.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python a.py 'base_##r1##_##r2##_##r3##_##r4##_##r5##_##r6##_##r7##.txt' all_content
+python a.py 'base_##r1##_##r2##_##r3##_##r4##_##r5##_##r6##_##r7##_##r8##.txt' all_content
